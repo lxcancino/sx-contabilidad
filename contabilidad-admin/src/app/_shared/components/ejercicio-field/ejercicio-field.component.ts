c72919675f47a5703c8cf14f6e59dd05a0421222
Ruben Cancino <luxsoft.cancino@gmail.com>
2021-01-05 09:59:56 -0600

    Update ejercicio-field.component.ts

--- contabilidad-admin/src/app/_shared/components/ejercicio-field/ejercicio-field.component.ts
+++ contabilidad-admin/src/app/_shared/components/ejercicio-field/ejercicio-field.component.ts
@@ -26,7 +26,7 @@ export class EjercicioFieldComponent implements OnInit {
   @Input()
   parent: FormGroup;
 
-  years = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
+  years = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026];
 
   constructor() {}
 
